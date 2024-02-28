bb80a068ddd161a09c5fadd76913f2e3d7b5885c
Maria Jose Molina <marymolina1301@gmail.com>
2024-02-28 21:49:00 +0100

    1st level complete

--- my-react-app/src/components/MemesGenerator.jsx
+++ my-react-app/src/components/MemesGenerator.jsx
@@ -1,42 +1,60 @@
 import { useEffect,useState } from "react";
+import '../App.css'
 
 const MemesGenerator = () => {
     const [memes, setMemes] = useState([]);
   const [currentMeme, setCurrentMeme] = useState([]);
   const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const currentIndex = memes.indexOf(currentMeme);
 
     useEffect(() => {
         fetch('https://api.imgflip.com/get_memes')
           .then(res => res.json())
           .then(data => {
             setMemes(data.data.memes);
-            setCurrentMeme(data.data.memes[0]);
+            const randomImageMeme = Math.floor(Math.random() * data.data.memes.length);
+            setCurrentMeme(data.data.memes[randomImageMeme]);
           })
           .catch(error => console.error('Error fetching memes:', error));
       }, []);
 
-      const randomImageMeme=() => {
-        const randomIndex = Math.floor(Math.random() * memes.length);
-        let url = randomIndex;
-        setCurrentMeme(memes[randomIndex]);
+
+      const handleRandom= () => {
+        const randomImageMeme = Math.floor(Math.random() * memes.length);
+        setCurrentMeme(memes[randomImageMeme]);
+      }
+      const handlePrev = () => {
+        const prevIndex = (currentIndex - 1) ;
+        setCurrentMeme(memes[prevIndex]);
+      };
+      
+      const handleNext = () => {
+        const nextIndex = (currentIndex + 1) ;
+        setCurrentMeme(memes[nextIndex]);
       };
 
-      const handchange= (e) => {
-        const {name, value} = e.target;
-        setCurrentMeme({...currentMeme, [name]: value});
-      }
     return ( 
         <>
-        <div>
+        <div >
         <input 
         type="text" 
         value={memes.topText} 
         onChange={(e) => setTopText(e.target.value)} />
-            <button></button>
+        <input 
+        type="text" 
+        value={memes.bottomText} 
+        onChange={(e) => setBottomText(e.target.value)} />
+        <button onClick={handleRandom}>Random Image</button>
+        <button onClick={handlePrev}>previous</button>
+        <button onClick={handleNext}>next</button>
         </div>
         <div>
-            <div>
+            <div className="memestext">
                 {topText} 
+            </div>
+            <div className="memestextBottiom">
+                {bottomText} 
             </div>
             <img src={currentMeme.url} alt="" />
         </div>
