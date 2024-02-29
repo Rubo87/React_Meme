5d14cbee502cb967c19ada2d28e48ad585b096b3
Ruben Otero <ruben87sp@gmail.com>
2024-02-29 20:46:14 +0100

    nothing

--- my-react-app/src/components/MemesGenerator.jsx
+++ my-react-app/src/components/MemesGenerator.jsx
@@ -11,6 +11,7 @@ const MemesGenerator = () => {
   const [bottomText, setBottomText] = useState('');
   const currentIndex = memes.indexOf(currentMeme);
   const elementRef = useRef(null);
+  const [file, setFile] = useState();
 
     useEffect(() => {
         fetch('https://api.imgflip.com/get_memes')
@@ -29,18 +30,20 @@ const MemesGenerator = () => {
         setCurrentMeme(memes[randomImageMeme]);
       }
       const handlePrev = () => {
-        const prevIndex = (currentIndex - 1) ;
+        const prevIndex = (currentIndex - 1) % memes.length;
         setCurrentMeme(memes[prevIndex]);
       };
       
       const handleNext = () => {
-        const nextIndex = (currentIndex + 1) ;
+        const nextIndex = (currentIndex + 1) % memes.length;
         setCurrentMeme(memes[nextIndex]);
       };
 
-        const downloadImage = () => {
-          saveAs(currentMeme.url , 'image.jpg') // Put your image URL here.
-        }
+      const handleImageUpload = (e) => {
+        console.log(e.target.files);
+        setFile(URL.createObjectURL(e.target.files[0]));
+        localStorage.setItem("selectedImage", e.target.files);
+      };
       
         const htmlToImageConvert = () => {
           toPng(elementRef.current, { cacheBust: false })
@@ -66,6 +69,7 @@ const MemesGenerator = () => {
         type="text" 
         value={memes.bottomText} 
         onChange={(e) => setBottomText(e.target.value)} />
+                <input type="file" onChange={handleImageUpload} />
         <button onClick={handleRandom}>Random Image</button>
         <button onClick={handlePrev}>previous</button>
         <button onClick={handleNext}>next</button>
@@ -78,7 +82,8 @@ const MemesGenerator = () => {
             <div className="memestextBottiom">
                 {bottomText} 
             </div>
-            <img src={currentMeme.url} alt="" />
+            {file ? <img src={file} alt="Uploaded Image" /> : <img className="image-container" src={currentMeme.url} alt="" />}
+      
         </div>
         </>
      );
